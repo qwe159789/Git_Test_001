b2b5ccb53b8698c91451742441f3720b6511653c
nuhuolei <nuhuolei@qq>
2022-06-21 05:07:16 +0800

    third commit

--- src/main/java/com/cj/git/GitTest.java
+++ src/main/java/com/cj/git/GitTest.java
@@ -3,5 +3,8 @@ package com.cj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git1!");
+        System.out.println("Hello git2!");
+        System.out.println("Hello git3!");
     }
 }
